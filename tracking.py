76ae94d31ffbf8e471ffc2f95f8ab54d5b3afec4
devs1man <devikamanoharan.6@gmail.com>
2026-01-21 19:47:21 +0530

    loading video with argsparse

--- tracking.py
+++ tracking.py
@@ -1,95 +1,105 @@
+import argparse
 import cv2
 import json
 from ultralytics import YOLO
 
-INPUT_VIDEO_PATH = "input.mp4"
-OUTPUT_VIDEO_PATH = "output_tracked.mp4"
-OUTPUT_JSON_PATH = "tracking_log.json"
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video", type=str, required=True, help="Path to input video file")
+    parser.add_argument("--conf", type=float, default=0.5, help="Confidence threshold")
+    args = parser.parse_args()
 
-MODEL_NAME = "yolov8n.pt"
-CONF_THRESHOLD = 0.5
+    INPUT_VIDEO_PATH = args.video
+    CONF_THRESHOLD = args.conf
 
-model = YOLO(MODEL_NAME)
+    OUTPUT_VIDEO_PATH = "output_tracked.mp4"
+    OUTPUT_JSON_PATH = "tracking_log.json"
+    MODEL_NAME = "yolov8n.pt"
 
-cap = cv2.VideoCapture(INPUT_VIDEO_PATH)
+    model = YOLO(MODEL_NAME)
 
-if not cap.isOpened():
-    print("Error could not open the video folder")
-    exit()
+    cap = cv2.VideoCapture(INPUT_VIDEO_PATH)
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    if not cap.isOpened():
+        print("Error could not open the video folder")
+        exit()
 
-print("Video opened successfully")
-print(f"FPS: {fps}, width:{width}, height: {height}")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter(OUTPUT_VIDEO_PATH, fourcc, fps, (width, height))
+    print("Video opened successfully")
+    print(f"FPS: {fps}, width:{width}, height: {height}")
 
-track_log = []
-frame_count = 0
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(OUTPUT_VIDEO_PATH, fourcc, fps, (width, height))
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    track_log = []
+    frame_count = 0
 
-    frame_count+= 1
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    time_sec = frame_count / fps
+        frame_count+= 1
 
-    results = model.track(frame, persist=True, tracker = "bytetrack.yaml", conf = CONF_THRESHOLD)
-    r = results[0]
+        time_sec = frame_count / fps
 
-    frame_tracks = [0]
+        results = model.track(frame, persist=True, tracker = "bytetrack.yaml", conf = CONF_THRESHOLD)
+        r = results[0]
 
-    if r.boxes is not None and len(r.boxes)>0:
-        ids = r.boxes.id
+        frame_tracks = [0]
 
-        for i in range(len(r.boxes)):
-            box = r.boxes[i]
+        if r.boxes is not None and len(r.boxes)>0:
+            ids = r.boxes.id
 
-            conf = float(box.conf[0])
-            cls_id = int(box.cls[0])
-            class_name = model.names[cls_id]
+            for i in range(len(r.boxes)):
+                box = r.boxes[i]
 
-            x1, y1, x2, y2 = box.xyxy[0].tolist()
-            bbox = [int(x1), int(y1), int(x2), int(y2)]
+                conf = float(box.conf[0])
+                cls_id = int(box.cls[0])
+                class_name = model.names[cls_id]
 
-            track_id = None     
-            if ids is not None:
-                track_id = int(ids[i].item())    
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+                bbox = [int(x1), int(y1), int(x2), int(y2)]
 
-            frame_tracks.append({
+                track_id = None     
+                if ids is not None:
+                    track_id = int(ids[i].item())    
+
+                frame_tracks.append({
                 "track_id" : track_id,
                 "class" : class_name,
                 "conf" : round(conf,3),
                 "bbox" : bbox
             }) 
         
-        track_log.append({
+            track_log.append({
             "frame" : frame_count,
             "time_sec": round(time_sec, 3),
             "tracks" : frame_tracks
         })
 
-        tracked_frame = r.plot()
-        out.write(tracked_frame)
+            tracked_frame = r.plot()
+            out.write(tracked_frame)
 
-        cv2.imshow("Step 3- YOLO tracking", tracked_frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-        if frame_count % 30 == 0:
-            print(f"Processed {frame_count} frames...")
+            cv2.imshow("Step 3- YOLO tracking", tracked_frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            if frame_count % 30 == 0:
+                print(f"Processed {frame_count} frames...")
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-with open(OUTPUT_JSON_PATH, "w") as f:
-    json.dump(track_log,f, indent=2)
+    with open(OUTPUT_JSON_PATH, "w") as f:
+        json.dump(track_log,f, indent=2)
 
-print("DONE SUCCESSFULLY!")
-print("saved tracked video: ",OUTPUT_VIDEO_PATH)
-print("saved tracked logs: ", OUTPUT_JSON_PATH)
+    print("DONE SUCCESSFULLY!")
+    print("saved tracked video: ",OUTPUT_VIDEO_PATH)
+    print("saved tracked logs: ", OUTPUT_JSON_PATH)
+
+if __name__ == "__main__":
+    main()
